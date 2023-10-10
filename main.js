7cb5fffff42f39353c5eea912af9030ab639e690
rollinsbaird <rmbaird@crimson.ua.edu>
2023-10-10 15:29:10 -0500

    update content

--- main.js
+++ main.js
@@ -1,35 +1,39 @@
-import './style.css'
+import "./style.css";
 
-import * as THREE from 'three';
+import * as THREE from "three";
 
-import { OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
 
-
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
+  canvas: document.querySelector("#bg"),
 });
 
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
-renderer.render( scene, camera );
+renderer.render(scene, camera);
 
-const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 )
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 const material = new THREE.MeshNormalMaterial();
-const torus = new THREE.Mesh( geometry, material );
+const torus = new THREE.Mesh(geometry, material);
 
-scene.add(torus)
+scene.add(torus);
 
-const pointLight = new THREE.PointLight(0xffffff)
-pointLight.position.set(5,5,5)
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(pointLight, ambientLight)
+scene.add(pointLight, ambientLight);
 
 // const lightHelper = new THREE.PointLightHelper(pointLight)
 // const gridHelper = new THREE.GridHelper(200, 50);
@@ -38,20 +42,21 @@ scene.add(pointLight, ambientLight)
 const controls = new OrbitControls(camera, renderer.domElement);
 
 function addStar() {
-  const geometry = new THREE.SphereGeometry( 0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial( { color: 0xffffff })
-  const star = new THREE.Mesh( geometry, material );
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
 
-  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ) );
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
 
   star.position.set(x, y, z);
-  scene.add(star)
+  scene.add(star);
 }
 
-Array(197).fill().forEach(addStar)
+Array(197).fill().forEach(addStar);
 
-
-const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+const spaceTexture = new THREE.TextureLoader().load("space.jpg");
 scene.background = spaceTexture;
 
 // const rollinsTexture = new THREE.TextureLoader().load('rollins.png');
@@ -64,13 +69,13 @@ scene.background = spaceTexture;
 // rollins.position.set(17,10,9);
 // scene.add(rollins);
 
-const earthTexture = new THREE.TextureLoader().load('earth.jpg');
+const earthTexture = new THREE.TextureLoader().load("earth.jpg");
 
 const earth = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
-  new THREE.MeshStandardMaterial( {
+  new THREE.MeshStandardMaterial({
     map: earthTexture,
-  } )
+  })
 );
 
 earth.position.z = 30;
@@ -83,7 +88,7 @@ function moveCamera() {
   // earth.rotation.x += 0.05;
   earth.rotation.y += 0.075;
   // earth.rotation.z += 0.05;
-  
+
   // rollins.rotation.y += 0.01;
   // rollins.rotation.z += 0.01;
 
@@ -95,13 +100,13 @@ function moveCamera() {
 document.body.onscroll = moveCamera;
 
 function animate() {
-  requestAnimationFrame( animate );
+  requestAnimationFrame(animate);
 
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
-  renderer.render (scene, camera );
+  renderer.render(scene, camera);
 }
 
-animate()
+animate();
